6d0c28d7da206d7bb49d3b201eb8a9a81b26f363
aajobitaiwo <jobitaiwo@gmail.com>
2024-10-16 23:06:19 -0400

    updated jenkins installation script

--- installations/jenkins-install.sh
+++ installations/jenkins-install.sh
@@ -1,16 +1,16 @@
-# #!/bin/bash
-# # Hardware requirements: Amazon Linux 2 Kernel 5.10 AMI with mimum t2.medium type instance & port 8080(jenkins), 9100 (node-exporter) should be allowed on the security groups
-# #Note: Dont use the latest EC2 Linux of 2023 as it doesnt have amazon-linux-extras which was used in the Jenkins installation as per the old AMI.
-# # Installing Jenkins
-# sudo yum update –y
-# sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-# sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key # Note: Refer this link to change this key line frequently https://pkg.jenkins.io/redhat-stable/
-# sudo yum upgrade
-# sudo amazon-linux-extras install java-openjdk11 -y
-# sudo yum install jenkins -y
-# sudo echo "jenkins ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-# sudo systemctl enable jenkins
-# sudo systemctl start jenkins
+#!/bin/bash
+# Hardware requirements: Amazon Linux 2 Kernel 5.10 AMI with mimum t2.medium type instance & port 8080(jenkins), 9100 (node-exporter) should be allowed on the security groups
+#Note: Dont use the latest EC2 Linux of 2023 as it doesnt have amazon-linux-extras which was used in the Jenkins installation as per the old AMI.
+# Installing Jenkins
+sudo yum update –y
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key # Note: Refer this link to change this key line frequently https://pkg.jenkins.io/redhat-stable/
+sudo yum upgrade
+sudo amazon-linux-extras install java-openjdk11 -y
+sudo yum install jenkins -y
+sudo echo "jenkins ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
 
 #!/bin/bash
 # Ensure that your software packages are up to date on your instance by using the following command to perform a quick software update:
